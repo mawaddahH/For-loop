781cc1bb4551687b57585bce600266f2647b4ca7
lo0ol <lo0ol@MWDH-Work2>
2022-07-03 02:38:37 +0300

    First Commit

---
+++ For loop/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		
+		
+		for (char alphabet ='A';alphabet<='Z';alphabet++) {
+			System.out.print(alphabet+"  ");
+		}
+	}
+
+}
